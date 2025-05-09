7fb3f62f8bffe9caf9471f4d0740b2d31f761dc1
towawawa <108978090+towawawa@users.noreply.github.com>
2025-05-09 10:59:06 +0900

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://urlibrary-front.onrender.com'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
 
     'allowed_origins_patterns' => [],
 
